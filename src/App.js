2d66dc9893236d0aa7b7f067aa7e3786336737ed
yut-0315 <94541673+yuto-0315@users.noreply.github.com>
2025-06-14 13:54:37 +0900

    Deploy to GitHub Pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Navigation from './components/Navigation';
 import SoundCollection from './pages/SoundCollection';
